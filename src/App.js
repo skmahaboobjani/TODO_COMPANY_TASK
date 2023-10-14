2233b7eeb2e5ab76c3905fc9825f61d0be635668
mahaboob jani <skmabujani521@gmail.com>
2023-10-14 19:23:28 +0530

    mahaboob

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/1/todos')
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          title: newTodo,
+          completed: false,
+        },
+      ]);
+      setNewTodo('');
+    }
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id, newTitle) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, title: newTitle } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo} disabled={!newTodo.trim()}>
+        Add
+      </button>
+      <div className="filter-buttons">
+        <button  onClick={() => setShowCompleted(false)}>All</button>
+        <button onClick={() => setShowCompleted(true)}>Completed</button>
+      </div>
+      <ul>
+        {todos
+          .filter((todo) => (showCompleted ? todo.completed : true))
+          .map((todo) => (
+            <li key={todo.id}>
+              <div>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(todo.id)}
+                />
+                <span className={todo.completed ? 'completed' : ''}>
+                  {todo.title}
+                </span>
+              </div>
+              <div>
+                <button onClick={() => editTodo(todo.id, prompt('Edit task', todo.title))} className="edit-button">
+                  Edit
+                </button>
+                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+              </div>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
